ffc284860b0f7f9c1564ed400277363258616ab6
Wjinlei <1976883731@qq.com>
2020-08-24 16:09:36 +0800

    添加GenerateToken,ParseToken函数

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module mylib
 
 go 1.15
 
-require github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+)
---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	random "mylib/random"
+)
+
+var (
+	SecretKey = random.CreateRandomString(32)
+)
+
+type Claims struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(username, password string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(3 * time.Hour)
+	claims := Claims{
+		username,
+		password,
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "hws", //签名的发行者
+		},
+	}
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString([]byte(SecretKey))
+	return token, err
+}
+
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+	return nil, err
+}
