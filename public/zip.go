ccd5d464e0a4463804da8ed8857f4e998989baec
Wjinlei <1976883731@qq.com>
2020-08-26 21:49:00 +0800

    ZIP压缩解压缩支持中文，支持指定路径

--- public/zip.go
+++ public/zip.go
@@ -1,13 +1,18 @@
 package public
 
 import (
-	"github.com/yeka/zip"
+	"errors"
+	"fmt"
 	"io"
 	"os"
+	"strings"
+
+	"github.com/axgle/mahonia"
+	"github.com/yeka/zip"
 )
 
 // 不加密压缩
-func ZIP(srcpath string, destpath string) error {
+func ZIP(srcpath, destpath string) error {
 	src, err := os.Open(srcpath)
 	if err != nil {
 		return err
@@ -19,22 +24,22 @@ func ZIP(srcpath string, destpath string) error {
 	}
 	defer dest.Close()
 	// 包装dest
-	zipWriter := zip.NewWriter(dest)
-	defer zipWriter.Close()
-	ioWriter, err := zipWriter.Create(srcpath)
+	srcWriter := zip.NewWriter(dest)
+	defer srcWriter.Close()
+	destIoWriter, err := srcWriter.Create(srcpath)
 	if err != nil {
 		return err
 	}
-	_, err = io.Copy(ioWriter, src)
+	_, err = io.Copy(destIoWriter, src)
 	if err != nil {
 		return err
 	}
-	zipWriter.Flush()
+	srcWriter.Flush()
 	return nil
 }
 
 // 加密压缩
-func ZIPEncrypt(srcpath string, destpath string, key string) error {
+func ZIPEncrypt(srcpath, destpath, password string) error {
 	src, err := os.Open(srcpath)
 	if err != nil {
 		return err
@@ -46,16 +51,62 @@ func ZIPEncrypt(srcpath string, destpath string, key string) error {
 	}
 	defer dest.Close()
 	// 包装dest
-	zipWriter := zip.NewWriter(dest)
-	defer zipWriter.Close()
-	ioWriter, err := zipWriter.Encrypt(srcpath, key, zip.StandardEncryption)
+	srcWriter := zip.NewWriter(dest)
+	defer srcWriter.Close()
+	destIoWriter, err := srcWriter.Encrypt(srcpath, password, zip.StandardEncryption)
 	if err != nil {
 		return err
 	}
-	_, err = io.Copy(ioWriter, src)
+	_, err = io.Copy(destIoWriter, src)
 	if err != nil {
 		return err
 	}
-	zipWriter.Flush()
+	srcWriter.Flush()
+	return nil
+}
+
+// 解压缩
+func ZIPDecrypt(srcpath, destpath, password, charset string) error {
+	encoder := mahonia.NewEncoder(charset)
+	if encoder == nil {
+		return errors.New(fmt.Sprintf("Charset error: [%s]", charset))
+	}
+	password = encoder.ConvertString(password)
+	decoder := mahonia.NewDecoder(charset)
+	if decoder == nil {
+		return errors.New(fmt.Sprintf("Charset error: [%s]", charset))
+	}
+	readCloser, err := zip.OpenReader(srcpath)
+	if err != nil {
+		return err
+	}
+	defer readCloser.Close()
+	destpath = strings.TrimRight(destpath, "/")
+	for _, file := range readCloser.File {
+		if file.FileInfo().IsDir() {
+			continue
+		}
+		if file.IsEncrypted() {
+			file.SetPassword(password)
+		}
+		filepath := destpath + "/" + decoder.ConvertString(file.Name)
+		if err := MakeDirAll(filepath); err != nil {
+			return err
+		}
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+		dest, err := os.Create(filepath)
+		if err != nil {
+			return err
+		}
+		defer dest.Close()
+		_, err = io.Copy(dest, src)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
--- public/zip_test.go
+++ public/zip_test.go
@@ -3,13 +3,19 @@ package public
 import "testing"
 
 func TestZIP(t *testing.T) {
-	if err := ZIP("./.sayHello.sh", "./test1.zip"); err != nil {
+	if err := ZIP("./zip.go", "./test_nopassword.zip"); err != nil {
 		t.Error(err)
 	}
 }
 
 func TestZIPEncrypt(t *testing.T) {
-	if err := ZIPEncrypt("./.sayHello.sh", "./test2.zip", "123"); err != nil {
+	if err := ZIPEncrypt("./zip.go", "./test_password123.zip", "123"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestZIPDecrypt(t *testing.T) {
+	if err := ZIPDecrypt("./test_password123.zip", "tmp", "123", "utf-8"); err != nil {
 		t.Error(err)
 	}
 }
