2e5d26fe914d6514e6c5a457dffd33c6a83cdf51
Wjinlei <1976883731@qq.com>
2020-08-27 15:29:14 +0800

    支持压缩时指定编码

--- public/zip.go
+++ public/zip.go
@@ -13,7 +13,7 @@ import (
 )
 
 // 不加密压缩
-func ZIP(srcpath, destpath string) error {
+func ZIP(srcpath, destpath, encoding string) error {
 	// 判断传入的源路径是否是目录
 	ok, err := IsDir(srcpath)
 	if err != nil {
@@ -21,12 +21,12 @@ func ZIP(srcpath, destpath string) error {
 	}
 	if !ok {
 		// 文件
-		if err := ZIPFile(srcpath, destpath); err != nil {
+		if err := ZIPFile(srcpath, destpath, encoding); err != nil {
 			return err
 		}
 	} else {
 		// 目录
-		if err := ZIPDir(srcpath, destpath); err != nil {
+		if err := ZIPDir(srcpath, destpath, encoding); err != nil {
 			return err
 		}
 	}
@@ -34,7 +34,15 @@ func ZIP(srcpath, destpath string) error {
 }
 
 // 压缩文件
-func ZIPFile(srcpath, destpath string) error {
+func ZIPFile(srcpath, destpath, encoding string) error {
+	// 编码器
+	encoder := mahonia.NewEncoder(encoding)
+	if encoder == nil {
+		return errors.New("encoding error")
+	}
+	// 设置文件名编码
+	newPath := encoder.ConvertString(srcpath)
+	// 创建目标
 	dest, err := os.Create(destpath)
 	if err != nil {
 		return err
@@ -42,10 +50,12 @@ func ZIPFile(srcpath, destpath string) error {
 	defer dest.Close()
 	srcWriter := zip.NewWriter(dest)
 	defer srcWriter.Close()
-	destIoWriter, err := srcWriter.Create(srcpath)
+	// 使用编码后的文件名创建目标Writer
+	destIoWriter, err := srcWriter.Create(newPath)
 	if err != nil {
 		return err
 	}
+	// 但打开文件还是要用源路径,否则无法找到文件
 	src, err := os.Open(srcpath)
 	if err != nil {
 		return err
@@ -60,7 +70,13 @@ func ZIPFile(srcpath, destpath string) error {
 }
 
 // 压缩目录
-func ZIPDir(srcpath, destpath string) error {
+func ZIPDir(srcpath, destpath, encoding string) error {
+	// 编码器
+	encoder := mahonia.NewEncoder(encoding)
+	if encoder == nil {
+		return errors.New("encoding error")
+	}
+	// 创建目标
 	dest, err := os.Create(destpath)
 	if err != nil {
 		return err
@@ -68,6 +84,7 @@ func ZIPDir(srcpath, destpath string) error {
 	defer dest.Close()
 	srcWriter := zip.NewWriter(dest)
 	defer srcWriter.Close()
+	// 遍历目录并压缩
 	srcpath = strings.TrimRight(srcpath, "/")
 	filepath.Walk(srcpath, func(path string, info os.FileInfo, err error) error {
 		if path != srcpath {
@@ -76,6 +93,8 @@ func ZIPDir(srcpath, destpath string) error {
 			if info.IsDir() {
 				header.Name += "/"
 			}
+			// 设置文件名编码
+			header.Name = encoder.ConvertString(header.Name)
 			destIoWriter, _ := srcWriter.CreateHeader(header)
 			if !info.IsDir() {
 				file, _ := os.Open(path)
--- public/zip_test.go
+++ public/zip_test.go
@@ -3,13 +3,13 @@ package public
 import "testing"
 
 func TestZIP(t *testing.T) {
-	if err := ZIP("./zip.go", "./test_nopassword.zip"); err != nil {
+	if err := ZIP("./dir.go", "./test_nopassword.zip", "GBK"); err != nil {
 		t.Error(err)
 	}
 }
 
 func TestZIPDecrypt(t *testing.T) {
-	if err := ZIPDecrypt("./test_nopassword.zip", "/tmp", "", "utf-8"); err != nil {
+	if err := ZIPDecrypt("./test_nopassword.zip", "/tmp", "", "GBK"); err != nil {
 		t.Error(err)
 	}
 }
