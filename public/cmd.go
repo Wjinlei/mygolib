b55f6d5dbaea9e473b137207f74858ba80c42e40
Wjinlei <1976883731@qq.com>
2020-09-17 17:55:42 +0800

    优化cmd

--- public/cmd.go
+++ public/cmd.go
@@ -1,8 +1,6 @@
 package public
 
 import (
-	"bytes"
-	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -10,38 +8,24 @@ import (
 
 // ExecShell 执行一条命令
 func ExecShell(command string) (string, error) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
 	cmd := exec.Command("bash", "-c", command)
 	cmd.Env = getSysctrlEnv(os.Environ())
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	defer stdout.Reset()
-	defer stderr.Reset()
-	err := cmd.Run()
+	out, err := cmd.Output()
 	if err != nil {
-		errOut := strings.TrimSpace(stderr.String())
-		return fmt.Sprintf("[DEBUG]: %s [ERROR]: %s [INFO]: %s\n", command, err, errOut), err
+		return "", err
 	}
-	return strings.TrimSpace(stdout.String()), nil
+	return string(out), nil
 }
 
 // ExecScript 执行一个脚本
 func ExecScript(params ...string) (string, error) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
 	cmd := exec.Command("bash", params...)
 	cmd.Env = getSysctrlEnv(os.Environ())
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	defer stdout.Reset()
-	defer stderr.Reset()
-	err := cmd.Run()
+	out, err := cmd.Output()
 	if err != nil {
-		errOut := strings.TrimSpace(stderr.String())
-		return fmt.Sprintf("[DEBUG]: %s [ERROR]: %s [INFO]: %s\n", params, err, errOut), err
+		return "", err
 	}
-	return strings.TrimSpace(stdout.String()), nil
+	return string(out), nil
 }
 
 // DoSysctrl 执行sysctl
