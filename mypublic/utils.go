b665e91a2a3fe82f437d490d1cd655ca33f4a7fc
Wjinlei <1976883731@qq.com>
2020-10-19 20:54:34 +0800

    优化utils.go

--- mypublic/utils.go
+++ mypublic/utils.go
@@ -5,21 +5,29 @@ import (
 	"strings"
 )
 
-// PathExists 判断文件是否存在
-func PathExists(path string) bool {
-	if _, err := os.Stat(path); err == nil {
-		return true
+// 判断所给路径文件/文件夹是否存在
+func Exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
 	}
-	return false
+	return true
 }
 
-// IsDir 判断路径是否是文件夹
-func IsDir(path string) (bool, error) {
+// 判断所给路径是否为文件夹
+func IsDir(path string) bool {
 	s, err := os.Stat(path)
 	if err != nil {
-		return false, err
+		return false
 	}
-	return s.IsDir(), nil
+	return s.IsDir()
+}
+
+// 判断所给路径是否为文件
+func IsFile(path string) bool {
+	return !IsDir(path)
 }
 
 // ContainsString 判断元素是否存在于Slice中, String类型
