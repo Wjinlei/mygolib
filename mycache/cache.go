581b944b309661ab99b28b91c4644d94bba4d993
Wjinlei <1976883731@qq.com>
2020-12-29 17:37:45 +0800

    fix bug

--- mycache/cache.go
+++ mycache/cache.go
@@ -1,25 +1,29 @@
 package mycache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // 全局map
 var (
-	obj1 map[string]interface{} // 缓存对象,里面存放的是缓存的字符串
-	obj2 map[string]int64       // 缓存时间Map,里面存放的是缓存对象的缓存开始时间
-	obj3 map[string]int64       // 缓存过期时间,里面存放的是缓存对象的过期时间
+	mutex sync.Mutex
+	obj1  map[string]interface{} = make(map[string]interface{}) // 缓存对象,里面存放的是缓存的字符串
+	obj2  map[string]int64       = make(map[string]int64)       // 缓存时间Map,里面存放的是缓存对象的缓存开始时间
+	obj3  map[string]int64       = make(map[string]int64)       // 缓存过期时间,里面存放的是缓存对象的过期时间
 )
 
 // Set 设置缓存, timeout = 0 或 < 0 都表示立即过期
 func Set(key string, value interface{}, timeout int64) {
-	initObj()                     // 初始化
+	mutex.Lock()
 	obj1[key] = value             // 设置缓存对象
 	obj2[key] = time.Now().Unix() // 设置缓存开始时间
 	obj3[key] = timeout           // 设置缓存超时时间
+	mutex.Unlock()
 }
 
 // Get 获取缓存
 func Get(key string) interface{} {
-	initObj()
 	if _, ok := obj1[key]; !ok {
 		return nil
 	}
@@ -30,17 +34,4 @@ func Get(key string) interface{} {
 		return nil
 	}
 	return obj1[key]
-}
-
-// initObj 初始化对象
-func initObj() {
-	if obj1 == nil {
-		obj1 = make(map[string]interface{})
-	}
-	if obj2 == nil {
-		obj2 = make(map[string]int64)
-	}
-	if obj3 == nil {
-		obj3 = make(map[string]int64)
-	}
 }
