de659931b7ee43774ad70546903c0dada22ac367
Wjinlei <1976883731@qq.com>
2021-03-17 15:02:18 +0800

    optimized

--- myorm/logger.go
+++ myorm/logger.go
@@ -13,11 +13,7 @@ import (
 
 // newLogger 产生新的日志器对象
 func newLogger(logpath string, loglevel logger.LogLevel) (logger.Interface, error) {
-	rotator, err := newRotator(logpath)
-	if err != nil {
-		return nil, err
-	}
-	mylogger := logger.New(log.New(rotator, "\r\n", log.LstdFlags),
+	mylogger := logger.New(log.New(newRotator(logpath), "\r\n", log.LstdFlags),
 		logger.Config{
 			SlowThreshold: time.Second,
 			LogLevel:      loglevel,
@@ -28,19 +24,16 @@ func newLogger(logpath string, loglevel logger.LogLevel) (logger.Interface, erro
 }
 
 // newRotator 产生新的日志切割器
-func newRotator(logpath string) (*rotatelogs.RotateLogs, error) {
-	filePath := abs(logpath)
+func newRotator(logpath string) *rotatelogs.RotateLogs {
+	absPath := abs(logpath)
 	// 日志切割器,日志文件最长保留3小时,每小时滚动一次
-	rotator, err := rotatelogs.New(
-		fmt.Sprintf("%s-%s", filePath, "%Y%m%d%H%M"),
-		rotatelogs.WithLinkName(filePath),
+	rotator, _ := rotatelogs.New(
+		fmt.Sprintf("%s-%s", absPath, "%Y%m%d%H%M"),
+		rotatelogs.WithLinkName(absPath),
 		rotatelogs.WithMaxAge(time.Duration(3*3600)*time.Second),     // 日志文件清理前的最长保存时间
 		rotatelogs.WithRotationTime(time.Duration(3600)*time.Second), // 多久滚动一次
 	)
-	if err != nil {
-		return nil, err
-	}
-	return rotator, nil
+	return rotator
 }
 
 func abs(logpath string) string {
