5819bf6573153346e1a2a5c0c2625d5b2c678fef
Wjinlei <1976883731@qq.com>
2021-05-21 15:08:39 +0800

    optimized

--- mypublic/cmd.go
+++ mypublic/cmd.go
@@ -3,12 +3,27 @@ package mypublic
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 // ExecShell 执行一条命令
 func ExecShell(command string) (string, error) {
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	out, err := cmd.CombinedOutput()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] ExecShell: %s\n%s", command, out))
+	}
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), nil
+}
+
+// ExecShell 执行一条命令,不去除本地化内容
+func ExecShell2(command string) (string, error) {
 	cmd := exec.Command("bash", "-c", command)
 	//cmd.Env = GetSysctrlEnv(os.Environ())
 	out, err := cmd.CombinedOutput()
@@ -24,6 +39,20 @@ func ExecShell(command string) (string, error) {
 // ExecScript 执行一个脚本
 func ExecScript(params ...string) (string, error) {
 	cmd := exec.Command("bash", params...)
+	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	out, err := cmd.CombinedOutput()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] ExecScript: %s\n%s", strings.Join(params, " "), out))
+	}
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), nil
+}
+
+// ExecScript2 执行一个脚本,不去除本地化内容
+func ExecScript2(params ...string) (string, error) {
+	cmd := exec.Command("bash", params...)
 	//cmd.Env = GetSysctrlEnv(os.Environ())
 	out, err := cmd.CombinedOutput()
 	if Exists("debug") {
@@ -37,6 +66,19 @@ func ExecScript(params ...string) (string, error) {
 
 func RunCmd(name string, params ...string) (string, error) {
 	cmd := exec.Command(name, params...)
+	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	out, err := cmd.CombinedOutput()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] Run: %s %s\n%s", name, strings.Join(params, " "), out))
+	}
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), nil
+}
+
+func RunCmd2(name string, params ...string) (string, error) {
+	cmd := exec.Command(name, params...)
 	//cmd.Env = GetSysctrlEnv(os.Environ())
 	out, err := cmd.CombinedOutput()
 	if Exists("debug") {
@@ -49,11 +91,26 @@ func RunCmd(name string, params ...string) (string, error) {
 }
 
 func StartCmd(name string, params ...string) error {
+	cmd := exec.Command(name, params...)
+	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	err := cmd.Start()
 	if Exists("debug") {
 		log.Println(fmt.Sprintf("[DEBUG] Start: %s %s", name, strings.Join(params, " ")))
 	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func StartCmd2(name string, params ...string) error {
 	cmd := exec.Command(name, params...)
-	if err := cmd.Start(); err != nil {
+	//cmd.Env = GetSysctrlEnv(os.Environ())
+	err := cmd.Start()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] Start: %s %s", name, strings.Join(params, " ")))
+	}
+	if err != nil {
 		return err
 	}
 	return nil
