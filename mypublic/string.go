3069f62554f89ac0b8b2136e1c4d5f8d530e4f7a
Wjinlei <1976883731@qq.com>
2021-05-26 15:29:03 +0800

    optimized

--- mypublic/string.go
+++ mypublic/string.go
@@ -121,6 +121,7 @@ func GetPath(path string) string {
 
 func Escape(s string) string {
 	s = strings.ReplaceAll(s, "\\", "\\\\")
+	s = strings.ReplaceAll(s, "/", "\\/")
 	s = strings.ReplaceAll(s, "^", "\\^")
 	s = strings.ReplaceAll(s, "$", "\\$")
 	s = strings.ReplaceAll(s, "*", "\\*")
