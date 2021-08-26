3c959ec28a0022d9e019004be261503acc4aeeb2
Wjinlei <1976883731@qq.com>
2021-08-26 11:16:44 +0800

    Optimized

--- mypublic/cmd.go
+++ mypublic/cmd.go
@@ -36,6 +36,34 @@ func ExecShell2(command string) (string, error) {
 	return string(out), nil
 }
 
+// StartShell 执行一条命令,不等待结果
+func StartShell(command string) error {
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	err := cmd.Start()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] StartShell: %s\n", command))
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// StartShell 执行一条命令,不等待结果
+func StartShell2(command string) error {
+	cmd := exec.Command("bash", "-c", command)
+	//cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	err := cmd.Start()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] StartShell: %s\n", command))
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // ExecScript 执行一个脚本
 func ExecScript(params ...string) (string, error) {
 	cmd := exec.Command("bash", params...)
@@ -62,6 +90,32 @@ func ExecScript2(params ...string) (string, error) {
 		return string(out), err
 	}
 	return string(out), nil
+}
+
+func StartScript(params ...string) error {
+	cmd := exec.Command("bash", params...)
+	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
+	err := cmd.Start()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] StartScript: %s\n", strings.Join(params, " ")))
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func StartScript2(params ...string) error {
+	cmd := exec.Command("bash", params...)
+	//cmd.Env = GetSysctrlEnv(os.Environ())
+	err := cmd.Start()
+	if Exists("debug") {
+		log.Println(fmt.Sprintf("[DEBUG] StartScript: %s\n", strings.Join(params, " ")))
+	}
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func RunCmd(name string, params ...string) (string, error) {
@@ -109,34 +163,6 @@ func StartCmd2(name string, params ...string) error {
 	err := cmd.Start()
 	if Exists("debug") {
 		log.Println(fmt.Sprintf("[DEBUG] Start: %s %s\n", name, strings.Join(params, " ")))
-	}
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// StartShell 执行一条命令,不等待结果
-func StartShell(command string) error {
-	cmd := exec.Command("bash", "-c", command)
-	cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
-	err := cmd.Start()
-	if Exists("debug") {
-		log.Println(fmt.Sprintf("[DEBUG] StartShell: %s\n", command))
-	}
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// StartShell 执行一条命令,不等待结果
-func StartShell2(command string) error {
-	cmd := exec.Command("bash", "-c", command)
-	//cmd.Env = GetSysctrlEnv(os.Environ()) // 去除所有本地化的设置,让命令可以正确执行
-	err := cmd.Start()
-	if Exists("debug") {
-		log.Println(fmt.Sprintf("[DEBUG] StartShell: %s\n", command))
 	}
 	if err != nil {
 		return err
